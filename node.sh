2abb73ccab9fdfec40643e58edeca9f94b664242
MeowZauki <mr.sharifi285.wow@gmail.com>
2025-10-03 00:35:23 +0330

    node.sh

--- node.sh
+++ node.sh
@@ -6,34 +6,30 @@ GREEN='\033[1;32m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 clear
-# Step 1
-echo -e "${YELLOW}[1/8] Updating system...${NC}"
-apt update -y > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1 && apt-get update -y > /dev/null 2>&1
-echo -e "${GREEN}✅ System updated.${NC}"
 
-# Step 2
-echo -e "${YELLOW}[2/8] Installing required packages...${NC}"
+# Step 1
+echo -e "${YELLOW}[1/7] Installing required packages...${NC}"
 apt-get install -y curl socat git > /dev/null 2>&1
 echo -e "${GREEN}✅ Required packages installed.${NC}"
 
-# Step 3
-echo -e "${YELLOW}[3/8] Installing Docker...${NC}"
+# Step 2
+echo -e "${YELLOW}[2/8] Installing Docker...${NC}"
 curl -fsSL https://get.docker.com | sh > /dev/null 2>&1
 echo -e "${GREEN}✅ Docker installed.${NC}"
 
-# Step 4
-echo -e "${YELLOW}[4/8] Cloning Marzban-node repository...${NC}"
+# Step 3
+echo -e "${YELLOW}[3/7] Cloning Marzban-node repository...${NC}"
 git clone https://github.com/Gozargah/Marzban-node ~/Marzban-node > /dev/null 2>&1
 echo -e "${GREEN}✅ Repository cloned.${NC}"
 
-# Step 5
-echo -e "${YELLOW}[5/8] Creating data directory...${NC}"
+# Step 4
+echo -e "${YELLOW}[4/7] Creating data directory...${NC}"
 mkdir -p /var/lib/marzban-node > /dev/null 2>&1
 cd ~/Marzban-node || exit
 echo -e "${GREEN}✅ Data directory ready.${NC}"
 
-# Step 6
-echo -e "${YELLOW}[6/8] Creating docker-compose.yml...${NC}"
+# Step 5
+echo -e "${YELLOW}[5/7] Creating docker-compose.yml...${NC}"
 cat > docker-compose.yml <<EOL
 services:
   marzban-node:
@@ -51,15 +47,15 @@ services:
 EOL
 echo -e "${GREEN}✅ docker-compose.yml created.${NC}"
 
-# Step 7
-echo -e "${YELLOW}[7/8] Waiting for Certificate input...${NC}"
+# Step 6
+echo -e "${YELLOW}[6/7] Waiting for Certificate input...${NC}"
 echo -e "${CYAN}Please paste your Certificate content (from -----BEGIN CERTIFICATE----- to -----END CERTIFICATE-----).${NC}"
 echo -e "${CYAN}When finished, press ENTER and then Ctrl+D.${NC}"
 cat > /var/lib/marzban-node/ssl_client_cert.pem
 echo -e "${GREEN}✅ Certificate saved.${NC}"
 
-# Step 8
-echo -e "${YELLOW}[8/8] Starting Docker Compose...${NC}"
+# Step 7
+echo -e "${YELLOW}[7/7] Starting Docker Compose...${NC}"
 docker compose up -d > /dev/null 2>&1
 echo -e "${GREEN}✅ Docker Compose started.${NC}"
 
